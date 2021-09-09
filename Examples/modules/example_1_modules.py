5fabeac7da018ef4218b9067844ede3d4842162a
ariannedee <ariannedee@gmail.com>
2021-09-09 12:27:42 -0700

    Rename import alias. Print paths on new lines.

--- Examples/modules/example_1_modules.py
+++ Examples/modules/example_1_modules.py
@@ -1,5 +1,5 @@
 from package import package_func
-from package.package_module import package_module_func as pmc
+from package.package_module import package_module_func as pmf
 from package.subpackage import sub_module_func
 
 
@@ -10,8 +10,9 @@ def func():
 if __name__ == '__main__':
     func()
     package_func()
-    pmc()
+    pmf()
     sub_module_func()
 
     import sys
-    print(sys.path)  # Python searches for modules here (in-order)
+    for path in sys.path:
+        print(path)
